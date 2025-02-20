9f0f65b3c710b83a7d5e429079cb09f3f9784efe
danukarangith <danukarangith456@gmail.com>
2025-02-20 23:21:10 +0530

    route path add

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Dashboard from './components/student/Dashboard.tsx';
 import './styles/Dashboard.css';
 import SignIn from "./pages/SignIn.tsx";
 import SignUp from "./pages/SignUp.tsx";
+import NewResult from "./components/student/NewResult.tsx";
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/new-result" element={<NewResult />} />
                 {/* Add additional routes as needed */}
             </Routes>
         </Router>
