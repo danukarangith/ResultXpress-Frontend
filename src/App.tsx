c2e1cb7d184a2d5168ff1947073edf2aeebfea97
danukarangith <danukarangith456@gmail.com>
2025-02-23 11:04:19 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/student/Dashboard.tsx';
 import './styles/Dashboard.css';
-import SignIn from "./pages/SignIn.tsx";
-import SignUp from "./pages/SignUp.tsx";
+// import SignIn from "./pages/SignIn.tsx";
+// import SignUp from "./pages/SignUp.tsx";
 import NewResult from "./components/student/NewResult.tsx";
+import PreviousResult from "./components/student/PreviousResult.tsx";
+import './index.css';
+
 
 const App = () => {
     return (
@@ -12,9 +15,10 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/sign-up" element={<SignUp />} />
+                {/*<Route path="/sign-in" element={<SignIn />} />*/}
+                {/*<Route path="/sign-up" element={<SignUp />} />*/}
                 <Route path="/new-result" element={<NewResult />} />
+                <Route path="/previous-results" element={<PreviousResult />} />
                 {/* Add additional routes as needed */}
             </Routes>
         </Router>
---
+++ src/components/student/PreviousResultHeader.tsx
@@ -0,0 +1,25 @@
+
+import { FaSignInAlt, FaBell, FaUser } from 'react-icons/fa';
+
+const Header = () => {
+    return (
+        <div className="dashboard-header">
+            <div className="breadcrumb">
+                <span>Dashboard</span> / <span>Previous Result</span>
+            </div>
+            <h2>Previous Result Page</h2>
+            <div className="header-right">
+                <div className="search-bar">
+                    <input type="text" placeholder="Search" />
+                </div>
+                <div className="header-icons">
+                    <FaSignInAlt />
+                    <FaBell />
+                    <FaUser />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
