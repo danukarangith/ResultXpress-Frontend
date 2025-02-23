6eaf4ad699b1727df950bb55cfab374856a613f8
danukarangith <danukarangith456@gmail.com>
2025-02-24 03:27:25 +0530

    routes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/student/Dashboard.tsx';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import StudentDashboard from './components/student/StudentDashboard.tsx';
 import './styles/Dashboard.css';
-// import SignIn from "./pages/SignIn.tsx";
-// import SignUp from "./pages/SignUp.tsx";
 import NewResult from "./components/student/NewResult.tsx";
 import PreviousResult from "./components/student/PreviousResult.tsx";
-import './index.css';
-
+import DownloadReport from "./components/student/DownloadReport.tsx";
+import ProfileSettings from "./components/student/ProfileSetting.tsx";
+import LoginPage from "./pages/SignIn.tsx";
+import AdminDashboard from "./components/admin/AdminDashboard.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import ResultManage from "./components/admin/ResultManage.tsx";
+import ExcelUpload from "./components/admin/ExcelUpload.tsx";
+import StudentManage from "./components/admin/StudentManage.tsx";
+import AdminManage from "./components/admin/AdminManage.tsx";
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                {/*<Route path="/sign-in" element={<SignIn />} />*/}
-                {/*<Route path="/sign-up" element={<SignUp />} />*/}
-                <Route path="/new-result" element={<NewResult />} />
-                <Route path="/previous-results" element={<PreviousResult />} />
-                {/* Add additional routes as needed */}
+                {/* Public Route */}
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage />} />
+
+                {/* Student Protected Routes */}
+                <Route path="/student-dashboard" element={<ProtectedRoute element={<StudentDashboard />} allowedRole="STUDENT" />} />
+                <Route path="/new-result" element={<ProtectedRoute element={<NewResult />} allowedRole="STUDENT" />} />
+                <Route path="/previous-results" element={<ProtectedRoute element={<PreviousResult />} allowedRole="STUDENT" />} />
+                <Route path="/download-report" element={<ProtectedRoute element={<DownloadReport />} allowedRole="STUDENT" />} />
+                <Route path="/profile-settings" element={<ProtectedRoute element={<ProfileSettings />} allowedRole="STUDENT" />} />
+
+                {/* Admin Protected Routes */}
+                <Route path="/admin-dashboard" element={<ProtectedRoute element={<AdminDashboard />} allowedRole="ADMIN" />} />
+                <Route path="/result" element={<ProtectedRoute element={<ResultManage/>} allowedRole="ADMIN" />} />
+                <Route path="/excel" element={<ProtectedRoute element={<ExcelUpload/>} allowedRole="ADMIN" />} />
+                <Route path="/Students" element={<ProtectedRoute element={<StudentManage/>} allowedRole="ADMIN" />} />
+                <Route path="/admin" element={<ProtectedRoute element={<AdminManage/>} allowedRole="ADMIN" />} />
+
+
+
+                {/* Redirect unknown routes */}
+                <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </Router>
     );
